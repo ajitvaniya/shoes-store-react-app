42aa69f2ef4aec0d7f3d6ef00cf0924e5e2a12e8
ajitvaniya <vaniyaajit@gmail.com>
2024-10-18 20:23:16 +0530

    First Commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+        <div className="min-h-screen bg-gray-100 flex items-center justify-center">
+          <h1 className="text-4xl font-bold text-blue-600">
+            Shoes Store React App
+          </h1>
+        </div>
+    </>
+  )
+}
+
+export default App
